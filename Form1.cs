edf4e709c06780f340d097f3b19893e877bee9ed
Raychan69 <fasyaraihan82@gmail.com>
2023-03-05 23:40:51 +0700

    tambahan fitur GUI

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,20 @@ namespace tpmodul3_1302213116
         {
             InitializeComponent();
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "hallo " + textBox1.Text; 
+        }
     }
 }
